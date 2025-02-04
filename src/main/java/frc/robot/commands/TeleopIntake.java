61aa2c65472ef89dfd8b780d24ee0efa19179902
liujip0 <136777216+liujip0@users.noreply.github.com>
2025-02-04 18:31:45 -0500

    changed OI.intakeIn() and OI.intakeOut() to OI.getIntakePower()

--- src/main/java/frc/robot/commands/TeleopIntake.java
+++ src/main/java/frc/robot/commands/TeleopIntake.java
@@ -15,13 +15,7 @@ public class TeleopIntake extends Command {
   @Override
   public void execute() {
     // TODO: might have to invert intake speeds/directions
-    if (OI.intakeIn() > 0) {
-      intake.setSpeed(Math.pow(OI.intakeIn(), 2));
-    } else if (OI.intakeOut() > 0) {
-      intake.setSpeed(-1 * Math.pow(OI.intakeOut(), 2));
-    } else {
-      intake.setSpeed(0);
-    }
+    intake.setSpeed(Math.copySign(Math.pow(OI.getIntakePower(), 2), OI.getIntakePower()));
   }
 
   public boolean isFinished() {
