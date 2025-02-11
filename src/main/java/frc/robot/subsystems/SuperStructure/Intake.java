e66bfbf27682380bdd8dfc6a47411f4b555b7a4d
Kennan <kennanhunter5@gmail.com>
2025-02-10 16:55:27 -0800

    add pdh distribution panel (#1)

--- src/main/java/frc/robot/subsystems/SuperStructure/Intake.java
+++ src/main/java/frc/robot/subsystems/SuperStructure/Intake.java
@@ -1,17 +1,14 @@
 package frc.robot.subsystems.SuperStructure;
 
-import org.littletonrobotics.junction.Logger;
-
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.OI;
+import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
   private SparkMax intakeMotor;
