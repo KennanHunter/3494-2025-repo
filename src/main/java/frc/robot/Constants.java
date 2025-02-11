190daae77e48814a429df0895f9f418e52c13d88
Kennan <kennanhunter5@gmail.com>
2025-02-11 18:15:52 -0500

    add mitocandria logging

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,7 @@
 
 package frc.robot;
 
+import au.grapplerobotics.MitoCANdria;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -42,36 +43,47 @@ public final class Constants {
     REPLAY
   }
 
-  public static class Presets{
-    public static double armIntake = 0.65;//-34.0;
-    public static double armCoral = 0.7;//-136.0;
-    public static double armOuttakeL2 = 0.224;//-111.0;
-    public static double armOuttakeL3 = 0.29;//-106.0;
+  public static class Mitocandria {
+    public static final int MITOCANDRIA_CAN_ID = 50;
+
+    public static final int LIMELIGHT_CHANNEL = MitoCANdria.MITOCANDRIA_CHANNEL_5VB;
+    public static final int PIGEON_CHANNEL = MitoCANdria.MITOCANDRIA_CHANNEL_ADJ;
+  }
+
+  public static class Presets {
+    public static double armIntake = 0.65; // -34.0;
+    public static double armCoral = 0.7; // -136.0;
+    public static double armOuttakeL2 = 0.224; // -111.0;
+    public static double armOuttakeL3 = 0.29; // -106.0;
 
     public static double armAlgeaL2 = 0.25;
-    public static double armAlgeaL3 = 0.25;//-116.0;
+    public static double armAlgeaL3 = 0.25; // -116.0;
 
     public static double liftIntake = 0;
     public static double liftOuttakeL2 = 20;
     public static double liftOuttakeL3 = 44.0;
-
   }
-  public static class OI{
+
+  public static class OI {
     public static int PRIMARY_CONTROLLER_PORT = 0;
   }
-  public static class Elevator{
+
+  public static class Elevator {
     public static int bottomMagSensorDIO = 9;
     public static int leaderMotor = 12;
     public static int followerMotor = 13;
   }
-  public static class Arm{
+
+  public static class Arm {
     public static int armMotor = 15;
     public static double manualPowerPOS = 0.015;
   }
-  public static class Intake{
+
+  public static class Intake {
     public static int intakeMotor = 14;
     public static double DEADBAND = 0.05;
   }
+
   public static class Drivetrain {
     public static double driveBaseRadius() {
       return Math.hypot(trackWidthX / 2.0, trackWidthY / 2.0);
@@ -113,7 +125,7 @@ public final class Constants {
     public static final Translation2d ampCenter =
         new Translation2d(Units.inchesToMeters(72.455), fieldWidth);
 
-    public static class Reef{
+    public static class Reef {
       //       6
       //     ----
       //  4 /    \  5
@@ -122,45 +134,47 @@ public final class Constants {
       // 2  \    / 3
       //     ----
       //      1
-      //Algea is 7
-      //Intake Station Left is 8
-      //Intake Station Right is 9
-      //NOTE: These side location aren't used to drive to they are just use to search for the nearest side (also these are for the blue side)
+      // Algea is 7
+      // Intake Station Left is 8
+      // Intake Station Right is 9
+      // NOTE: These side location aren't used to drive to they are just use to search for the
+      // nearest side (also these are for the blue side)
       public static final Translation2d[] sideLocations = {
-        new Translation2d(3.156, 4.030), //1
-        new Translation2d(3.823, 5.154),  //2
-        new Translation2d(3.776, 2.858), //3
-        new Translation2d(5.164, 5.156), //4
-        new Translation2d(5.170, 2.858), //5
-        new Translation2d(5.832, 4.047),//6
-        new Translation2d(6.001, 0.489),//7
-        new Translation2d(1.172, 1.00),//8
-        new Translation2d(1.172, 7.0)//9
-
-      };
-      
-      public static final Pose2d[] leftLocations = { //Placeholder currently using the center positions
-        new Pose2d(3.158, 4.2, new Rotation2d(0.0-Math.PI/2.0)), //1
-        new Pose2d(3.944, 5.265, new Rotation2d(-Math.PI/3.0-Math.PI/2.0)),  //2
-        new Pose2d(3.662, 2.952,new Rotation2d(Math.PI/3.0-Math.PI/2.0)), //3
-        new Pose2d(5.024, 5.243,new Rotation2d(-2*Math.PI/3.0-Math.PI/2.0)), //4
-        new Pose2d(5.025, 2.771,new Rotation2d(2*Math.PI/3.0-Math.PI/2.0)), //5
-        new Pose2d(5.832, 4.187, new Rotation2d(Math.PI-Math.PI/2.0)),//6
-        new Pose2d(6.001, 0.489, new Rotation2d(Math.PI/2.0-Math.PI/2.0)),//7
-        new Pose2d(0.646, 1.434, new Rotation2d(0.872665-Math.PI/2.0)),//8
-        new Pose2d(0.683, 6.619, new Rotation2d(-0.872665-Math.PI/2.0))//9
+        new Translation2d(3.156, 4.030), // 1
+        new Translation2d(3.823, 5.154), // 2
+        new Translation2d(3.776, 2.858), // 3
+        new Translation2d(5.164, 5.156), // 4
+        new Translation2d(5.170, 2.858), // 5
+        new Translation2d(5.832, 4.047), // 6
+        new Translation2d(6.001, 0.489), // 7
+        new Translation2d(1.172, 1.00), // 8
+        new Translation2d(1.172, 7.0) // 9
       };
 
-      public static final Pose2d[] rightLocations = { //Placeholder currently using the center positions
-        new Pose2d(3.157, 3.957, new Rotation2d(0.0-Math.PI/2.0)), //1
-        new Pose2d(3.7, 5.090, new Rotation2d(-Math.PI/3.0-Math.PI/2.0)),  //2
-        new Pose2d(3.971, 2.778,new Rotation2d(Math.PI/3.0-Math.PI/2.0)), //3
-        new Pose2d(5.313, 5.078,new Rotation2d(-2*Math.PI/3.0-Math.PI/2.0)), //4
-        new Pose2d(5.306, 2.968,new Rotation2d(2*Math.PI/3.0-Math.PI/2.0)), //5
-        new Pose2d(5.832, 3.868, new Rotation2d(Math.PI-Math.PI/2.0)),//6
-        new Pose2d(6.001, 0.489, new Rotation2d(Math.PI/2.0-Math.PI/2.0)),//7
-        new Pose2d(1.702, 0.613, new Rotation2d(0.872665-Math.PI/2.0)),//8
-        new Pose2d(1.698, 7.375, new Rotation2d(-0.872665-Math.PI/2.0))//9
+      public static final Pose2d[]
+          leftLocations = { // Placeholder currently using the center positions
+        new Pose2d(3.158, 4.2, new Rotation2d(0.0 - Math.PI / 2.0)), // 1
+        new Pose2d(3.944, 5.265, new Rotation2d(-Math.PI / 3.0 - Math.PI / 2.0)), // 2
+        new Pose2d(3.662, 2.952, new Rotation2d(Math.PI / 3.0 - Math.PI / 2.0)), // 3
+        new Pose2d(5.024, 5.243, new Rotation2d(-2 * Math.PI / 3.0 - Math.PI / 2.0)), // 4
+        new Pose2d(5.025, 2.771, new Rotation2d(2 * Math.PI / 3.0 - Math.PI / 2.0)), // 5
+        new Pose2d(5.832, 4.187, new Rotation2d(Math.PI - Math.PI / 2.0)), // 6
+        new Pose2d(6.001, 0.489, new Rotation2d(Math.PI / 2.0 - Math.PI / 2.0)), // 7
+        new Pose2d(0.646, 1.434, new Rotation2d(0.872665 - Math.PI / 2.0)), // 8
+        new Pose2d(0.683, 6.619, new Rotation2d(-0.872665 - Math.PI / 2.0)) // 9
+      };
+
+      public static final Pose2d[]
+          rightLocations = { // Placeholder currently using the center positions
+        new Pose2d(3.157, 3.957, new Rotation2d(0.0 - Math.PI / 2.0)), // 1
+        new Pose2d(3.7, 5.090, new Rotation2d(-Math.PI / 3.0 - Math.PI / 2.0)), // 2
+        new Pose2d(3.971, 2.778, new Rotation2d(Math.PI / 3.0 - Math.PI / 2.0)), // 3
+        new Pose2d(5.313, 5.078, new Rotation2d(-2 * Math.PI / 3.0 - Math.PI / 2.0)), // 4
+        new Pose2d(5.306, 2.968, new Rotation2d(2 * Math.PI / 3.0 - Math.PI / 2.0)), // 5
+        new Pose2d(5.832, 3.868, new Rotation2d(Math.PI - Math.PI / 2.0)), // 6
+        new Pose2d(6.001, 0.489, new Rotation2d(Math.PI / 2.0 - Math.PI / 2.0)), // 7
+        new Pose2d(1.702, 0.613, new Rotation2d(0.872665 - Math.PI / 2.0)), // 8
+        new Pose2d(1.698, 7.375, new Rotation2d(-0.872665 - Math.PI / 2.0)) // 9
       };
     }
   }
