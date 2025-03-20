0551a3fac5888d200cb6024c6dcfc6802e51fea0
klees06 <kleesill12@gmail.com>
2025-03-20 17:47:00 -0400

    change swerve drive gear ratio to L1

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSparkMax.java
@@ -43,8 +43,8 @@ import java.util.Queue;
  * "/Drive/ModuleX/TurnAbsolutePositionRad"
  */
 public class ModuleIOSparkMax implements ModuleIO {
-  // Gear ratios for SDS MK4i L2, adjust as necessary
-  private static final double DRIVE_GEAR_RATIO = (50.0 / 14.0) * (17.0 / 27.0) * (45.0 / 15.0);
+  // Gear ratios for SDS MK4i L1, adjust as necessary
+  private static final double DRIVE_GEAR_RATIO = (50.0 / 14.0) * (19.0 / 25.0) * (45.0 / 15.0);
   private static final double TURN_GEAR_RATIO = 150.0 / 7.0;
 
   private final SparkFlex driveSparkFlex;
