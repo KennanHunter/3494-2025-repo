7379bc3408363b52e0bbf0290ae04e7a323c7ade
Kennan <kennanhunter5@gmail.com>
2025-03-21 23:28:58 -0400

    log all seen april tags

--- src/main/java/frc/robot/subsystems/limelights/Limelights.java
+++ src/main/java/frc/robot/subsystems/limelights/Limelights.java
@@ -1,19 +1,13 @@
 package frc.robot.subsystems.limelights;
 
-import java.util.Optional;
-import java.util.function.Supplier;
-
-import org.checkerframework.checker.units.qual.min;
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.math.estimator.ExtendedKalmanFilter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.numbers.N2;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.util.LimelightHelpers;
-
+import frc.robot.util.LimelightHelpers.RawFiducial;
+import java.util.Arrays;
+import org.littletonrobotics.junction.Logger;
 
 public class Limelights {
   private ExtendedKalmanFilter<N2, N2, N2> mKalmanFilter; // NICE IDEA IMPLEMENT LATER
@@ -28,63 +22,62 @@ public class Limelights {
 
   private boolean useMegatag1 = false;
   private LimelightHelpers.PoseEstimate limelightLeftMeasurment;
+
   public Limelights(Drive drivetrain, String limeLightName) {
     this.drivetrain = drivetrain;
     this.limelightName = limeLightName;
   }
 
   public void periodic() {
-    
-    // Use MegaTag2 because better?
-    //PLEASE FIX BIG PROBLEM, PRETTY SURE IMU ROTATION IS 90 DEGREES OFF, ALSO WHAT DOES THIS FUNCTION DO
-    //https://docs.limelightvision.io/docs/docs-limelight/pipeline-apriltag/apriltag-robot-localization-megatag2
+    try {
+      // Use MegaTag2 because better?
+      // PLEASE FIX BIG PROBLEM, PRETTY SURE IMU ROTATION IS 90 DEGREES OFF, ALSO WHAT DOES THIS
+      // FUNCTION DO
+      // https://docs.limelightvision.io/docs/docs-limelight/pipeline-apriltag/apriltag-robot-localization-megatag2
 
       LimelightHelpers.SetRobotOrientation(
-        limelightName, drivetrain.getPose().getRotation().getDegrees(), 0, 0, 0, 0, 0);
-      
-        limelightLeftMeasurment =
-        LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+          limelightName, drivetrain.getPose().getRotation().getDegrees(), 0, 0, 0, 0, 0);
 
+      limelightLeftMeasurment = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
 
-    if(useMegatag1 == true){
-      limelightLeftMeasurment =
-          LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
-    }
-    
-      
-    boolean leftLimelightEmpty = limelightLeftMeasurment == null;
-    boolean rotationRateTooHigh = drivetrain.rotationRate > 4.0 * Math.PI;
-    
-    boolean noTagsFound = true;
-    boolean tooFarAway = true;
-    if(!leftLimelightEmpty){
-      noTagsFound = limelightLeftMeasurment.tagCount() == 0;
-      tooFarAway = limelightLeftMeasurment.avgTagDist() > 3.1;// 3.25;
-    }
-    // Logger.recordOutput(limelightName+"/noTagsFound", noTagsFound);
-    // Logger.recordOutput(limelightName+"/tooFarAway", tooFarAway);
-    // Logger.recordOutput(limelightName+"/limelightLeftMeasurment", limelightLeftMeasurment);
-    
-    
-    if (leftLimelightEmpty || rotationRateTooHigh || noTagsFound || tooFarAway) {
-      // System.out.println(limelightName + "|" + leftLimelightEmpty + "|" + rotationRateTooHigh + "|" +  noTagsFound + "|" + limelightLeftMeasurment.avgTagDist());
-      validMeasurment = false;
-      // System.out.println("Invalid Measurement");
-      return;      
-    }
+      if (useMegatag1) {
+        limelightLeftMeasurment = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+      }
 
-    validMeasurment = true;
-    // Logger.recordOutput(limelightName+"/Valid", validMeasurment);
-    // System.out.println("READING-------------");
-    measurementTimeStamp = limelightLeftMeasurment.timestampSeconds();
-    measurementPosition = limelightLeftMeasurment.pose();
+      boolean leftLimelightEmpty = limelightLeftMeasurment == null;
+      boolean rotationRateTooHigh = drivetrain.rotationRate > 4.0 * Math.PI;
+
+      boolean noTagsFound = true;
+      boolean tooFarAway = true;
+
+      if (!leftLimelightEmpty) {
+        noTagsFound = limelightLeftMeasurment.tagCount() == 0;
+        tooFarAway = limelightLeftMeasurment.avgTagDist() > 3.1; // 3.25;
+      }
+
+      logAllFiducialsPose3d(limelightLeftMeasurment);
+      Logger.recordOutput(limelightName + "/Valid", validMeasurment);
+
+      if (leftLimelightEmpty || rotationRateTooHigh || noTagsFound || tooFarAway) {
+        validMeasurment = false;
+        return;
+      }
+
+      validMeasurment = true;
+
+      measurementTimeStamp = limelightLeftMeasurment.timestampSeconds();
+      measurementPosition = limelightLeftMeasurment.pose();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   // returns validity of last measurment
   public boolean measurmentValid() {
     return validMeasurment;
   }
-  public LimelightHelpers.PoseEstimate getMeasurement(){
+
+  public LimelightHelpers.PoseEstimate getMeasurement() {
     return limelightLeftMeasurment;
   }
 
@@ -95,22 +88,38 @@ public class Limelights {
   public Pose2d getMeasuremPosition() {
     return measurementPosition;
   }
+
   /**
-   * Sets the crop window for the camera. The crop window in the UI must be completely open.
-   * Notably has the side effect of resetting the X Crop
+   * Sets the crop window for the camera. The crop window in the UI must be completely open. Notably
+   * has the side effect of resetting the X Crop
+   *
    * @param minCrop Minimum Y value (-1 to 1)
    * @param maxCrop Maximum Y value (-1 to 1)
    */
-  public void setCropY(double minCrop, double maxCrop){
+  public void setCropY(double minCrop, double maxCrop) {
     LimelightHelpers.setCropWindow(limelightName, -1, 1, minCrop, maxCrop);
   }
 
   /**
-   * Sets the crop window for the camera. The crop window in the UI must be completely open.
-   * Notably has the side effect of resetting the X Crop
+   * Sets the crop window for the camera. The crop window in the UI must be completely open. Notably
+   * has the side effect of resetting the X Crop
+   *
    * @param megaTagStauts Sets which Megatag We pull from, TRUE = Megatag1, FALSE = Megatag2
    */
-  public void setMegatag(boolean megaTagStauts){
+  public void setMegatag(boolean megaTagStauts) {
     useMegatag1 = megaTagStauts;
+  }
+
+  private void logAllFiducialsPose3d(LimelightHelpers.PoseEstimate measurement) {
+    try {
+      RawFiducial[] asdf = limelightLeftMeasurment.rawFiducials();
+
+      Logger.recordOutput(
+          limelightName + "/SeenApriltags",
+          Arrays.stream(asdf).map(fid -> fid.id).toArray(String[]::new));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
