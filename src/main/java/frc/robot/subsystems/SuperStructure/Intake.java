c763d9bd4544db604e0e93b31ee5f99dabcf5644
liujip0 <liujip2020@gmail.com>
2025-03-24 20:06:48 -0400

    rename hasNote to hasCoral :)

--- src/main/java/frc/robot/subsystems/SuperStructure/Intake.java
+++ src/main/java/frc/robot/subsystems/SuperStructure/Intake.java
@@ -1,26 +1,27 @@
 package frc.robot.subsystems.SuperStructure;
 
+import java.util.ArrayList;
+
+import org.littletonrobotics.junction.Logger;
+
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-
-import java.util.ArrayList;
-
-import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
   private SparkMax intakeMotor;
   private SparkMaxConfig intakeConfig;
   private double intakeSpeed;
 
-  boolean hasNote = false;
+  boolean hasCoral = false;
   private double currentSpeed = 0;
-  ArrayList currents =  new ArrayList<>();
+  ArrayList<Double> currents = new ArrayList<>();
 
   public Intake() {
     Logger.recordOutput("Intake/Intake-Power", intakeSpeed);
@@ -37,36 +38,36 @@ public class Intake extends SubsystemBase {
     currentSpeed = speed;
     intakeMotor.set(speed);
   }
-  public double currentAverage(double currentCurrent){
+
+  public double currentAverage(double currentCurrent) {
     currents.add(0, currentCurrent);
 
-    if (currents.size() >= 20){
+    if (currents.size() >= 20) {
       currents.remove(currents.size() - 1);
     }
     double average = 0;
-    for(int i = 0; i<currents.size(); i++){
-      average+= (double)currents.get(i);
+    for (int i = 0; i < currents.size(); i++) {
+      average += (double) currents.get(i);
     }
     average /= currents.size();
     return average;
-      
   }
 
   @Override
-  public void periodic(){
+  public void periodic() {
     double Cavrg = currentAverage(intakeMotor.getOutputCurrent());
-    if(Cavrg >9){
-      hasNote = true;
+    if (Cavrg > 9) {
+      hasCoral = true;
     }
     Logger.recordOutput("Intake/Current-Average", Cavrg);
-    Logger.recordOutput("Intake/Has-Coral", hasNote);
-    Logger.recordOutput("Intake/Current-SPeed", currentSpeed);
-    if(currentSpeed > 0.05){
-      hasNote = false;
+    Logger.recordOutput("Intake/Has-Coral", hasCoral);
+    Logger.recordOutput("Intake/Current-Speed", currentSpeed);
+    if (currentSpeed > 0.05) {
+      hasCoral = false;
     }
   }
 
-  public boolean hasNote(){
-    return hasNote;
+  public boolean hasCoral() {
+    return hasCoral;
   }
 }
