43b2ea7498719f8b559c5c909788eabae23b5719
Kennan <kennanhunter5@gmail.com>
2025-04-05 23:27:46 -0400

    remove commands

--- src/main/java/frc/robot/commands/Direction.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.commands;
-
-public enum Direction {
-  CLOCKWISE,
-  COUNTER_CLOCKWISE;
-}
--- src/main/java/frc/robot/commands/DriveMode.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.commands;
-
-public enum DriveMode {
-  NORMAL_TELEOP,
-  AUTO_ALIGN
-}
