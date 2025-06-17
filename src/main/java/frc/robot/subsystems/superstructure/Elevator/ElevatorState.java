2f3998d79b0be26074b0904e54107ce708a52f25
Kennan <kennanhunter5@gmail.com>
2025-06-17 17:39:22 -0400

    start fsm

--- src/main/java/frc/robot/subsystems/superstructure/Elevator/ElevatorState.java
+++ src/main/java/frc/robot/subsystems/superstructure/Elevator/ElevatorState.java
@@ -1,6 +1,14 @@
 package frc.robot.subsystems.superstructure.Elevator;
 
+import static edu.wpi.first.units.Units.Meters;
+
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.units.measure.Distance;
 
-public record ElevatorState(Distance height, IdleMode idleMode) {}
+public record ElevatorState(Distance height, IdleMode idleMode) {
+  // TODO: I wrote this while talking to someone in my head that was insulting my code, does it make
+  // sense?
+  static ElevatorState holdAtMeters(double val) {
+    return new ElevatorState(Meters.of(val), IdleMode.kBrake);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/superstructure/KnownStates.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.superstructure;
+
+import static edu.wpi.first.units.Units.Meters;
+
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.subsystems.superstructure.Arm.ArmState;
+import frc.robot.subsystems.superstructure.Elevator.ElevatorState;
+import frc.robot.subsystems.superstructure.Intake.IntakeState;
+
+public enum KnownStates {
+  Test(
+      new SuperStructureState(
+          new ElevatorState(Meters.of(0.5), IdleMode.kBrake),
+          new ArmState(Rotation2d.kZero),
+          IntakeState.Hold));
+
+  KnownStates(SuperStructureState state) {}
+}
